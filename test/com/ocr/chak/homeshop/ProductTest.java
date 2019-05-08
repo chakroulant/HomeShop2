d3bc73892f78f64f1d4f80e20f0428df72a554de
chakroulant <valentin.mlb@gmail.com>
2019-05-08 20:06:13 +0200

    Added getCustomer getProducts and addProduct methods to the Bill class, the constructor Customer and Product

--- test/com/ocr/chak/homeshop/ProductTest.java
+++ test/com/ocr/chak/homeshop/ProductTest.java
@@ -1,7 +1,29 @@
 package com.ocr.chak.homeshop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+    private Customer customer;
+    private Map<Product, Integer> products;
+
+    /**
+     * Add a product with quantity in the bill
+     * @param product product to add
+     * @param quantity quantity of the product
+     */
+    public void addProduct (Product product, Integer quantity) {
+        this.products.put(product, quantity);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
 
 }
